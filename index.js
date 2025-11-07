859db0b1858cf99a1394c8c635ce19a12c3ecb3e
Larry <L.abbo@yahoo.com>
2025-11-07 16:07:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding: '2rem', fontFamily: 'sans-serif'}}>
+      <h1>Havenly</h1>
+      <p>A quiet place to check in with yourself.</p>
+    </main>
+  );
+}

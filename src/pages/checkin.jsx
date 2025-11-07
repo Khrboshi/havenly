6137460d10ab872768c93244c01cf6391599c70b
Larry <L.abbo@yahoo.com>
2025-11-07 16:23:58 +0200

    Create checkin.jsx

---
+++ src/pages/checkin.jsx
@@ -0,0 +1,8 @@
+export default function CheckIn() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h2>Check-In</h2>
+      <p>This is where we’ll build the reflection flow next.</p>
+    </main>
+  );
+}

03878c25a4d00f6c56c5918066782da8f4264e06
Larry <L.abbo@yahoo.com>
2025-11-12 15:33:32 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header style={{ padding: '20px', backgroundColor: '#111', color: '#fff' }}>
-      <h1>AI Innovation Hub</h1>
+      <h1>Havenly</h1>
       <nav>
         <a href="/" style={{ marginRight: '15px' }}>Home</a>
         <a href="/about" style={{ marginRight: '15px' }}>About</a>

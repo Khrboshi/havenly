06bb642091271347077df664e3397f48ad2d2283
Larry <L.abbo@yahoo.com>
2025-11-12 15:25:32 +0200

    Delete src/pages directory

--- src/pages/checkin.jsx
+++
@@ -1,8 +0,0 @@
-export default function CheckIn() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h2>Check-In</h2>
-      <p>This is where we’ll build the reflection flow next.</p>
-    </main>
-  );
-}

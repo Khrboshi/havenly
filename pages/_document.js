fb4fe00a49b9491068f69c592f2f2cc612798f75
Larry <L.abbo@yahoo.com>
2025-11-13 16:43:13 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -74,3 +74,21 @@ export default function Document() {
     </Html>
   );
 }
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=yes, viewport-fit=cover"
+        />
+      </Head>
+      <body className="bg-slate-50 text-slate-800 font-sans antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}

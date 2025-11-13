f3576be49c5635bf5d69ed1e1a1554d4fb494ee6
Larry <L.abbo@yahoo.com>
2025-11-13 15:01:24 +0200

    Update PageLoader.js

--- components/PageLoader.js
+++ components/PageLoader.js
@@ -7,7 +7,7 @@ export default function PageLoader() {
     <div className="fixed inset-0 flex items-center justify-center bg-white/80 backdrop-blur-sm z-[9999]">
       <motion.div
         animate={{ scale: [1, 1.3, 1], opacity: [1, 0.7, 1] }}
-        transition={{ repeat: Infinity, duration: 1.6, ease: "easeInOut" }}
+        transition={{ repeat: Infinity, duration: 1.8, ease: "easeInOut" }}
         className="w-4 h-4 bg-blue-500 rounded-full shadow-lg"
       />
     </div>
